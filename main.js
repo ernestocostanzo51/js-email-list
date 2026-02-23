837d57ad53db787360996de79ba0c1f0406af0cd
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-23 16:22:49 +0100

    ciclo per mettere mail

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+const listaEl = document.getElementById("lista")
 
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(res => res.json())
-.then(email => {
-    
-    for(i=0;i<10;i++){
-        console.log(email)
-    }
 
-})
+
+
+for (i=0; i<10; i++){
+   fetch("https://flynn.boolean.careers/exercises/api/random/mail") 
+   .then(res => res.json())
+   .then(data =>{
+    const mail = data.res
+    console.log(data)
+
+    const elementoLista = document.createElement("li")
+    listaEl.textContent = mail
+    listaEl.appendChild(elementoLista)
+
+
+   })
+}
